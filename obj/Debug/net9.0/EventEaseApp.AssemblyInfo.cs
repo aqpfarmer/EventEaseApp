7332cb1ed0068fd7410dc80eeedfd9d5a8d6bf5c
Chris Murphy <cmurphy@scmpeoc3t.army.mil>
2025-09-05 10:21:59 -0700

    More UI improvements and optiimizations

--- obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3438e9ed27cf729bd1e310140607684802882ecd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3351a84cde3d53c263194544a0a77bd0fb4e023")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
